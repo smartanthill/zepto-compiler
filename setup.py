3a55e58a21843e13936fdfdd15c357a511e6914f
Ivan Kravets <me@ikravets.com>
2015-07-18 13:16:53 +0300

    Temporary fix antlr4 to 4.5.1, 4.5.2 looks like broken package

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     url=__url__,
     license=__license__,
     install_requires=[
-        "antlr4-python2-runtime"
+        "antlr4-python2-runtime<=4.5.1"
     ],
     packages=find_packages(),
     classifiers=[
